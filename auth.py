97928cad23c370f2b3471e6b58958a9377cbd07a
Johannes <j.laute3@googlemail.com>
2018-04-15 16:35:11 +0100

    test

--- auth.py
+++ auth.py
@@ -4,6 +4,6 @@ from oauth2client.client import GoogleCredentials
 import getpass
 auth.authenticate_user()
 creds = GoogleCredentials.get_application_default()
-os.system(f"google-drive-ocamlfuse -headless -id={creds.client_id} -secret={creds.client_secret} < /dev/null 2>&1 | grep URL")
-vcode = getpass.getpass()
-os.system(f"echo {vcode} | google-drive-ocamlfuse -headless -id={creds.client_id} -secret={creds.client_secret}")
+# os.system(f"google-drive-ocamlfuse -headless -id={creds.client_id} -secret={creds.client_secret} < /dev/null 2>&1 | grep URL")
+# vcode = getpass.getpass()
+# os.system(f"echo {vcode} | google-drive-ocamlfuse -headless -id={creds.client_id} -secret={creds.client_secret}")
