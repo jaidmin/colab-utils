23b5d9a2b408111d7611a3efb4ff6ba414511a7f
Johannes <j.laute3@googlemail.com>
2018-04-15 16:51:55 +0100

    refactor

--- auth.py
+++ auth.py
@@ -2,8 +2,9 @@ from google.colab import auth
 import os
 from oauth2client.client import GoogleCredentials
 import getpass
-auth.authenticate_user()
-creds = GoogleCredentials.get_application_default()
-# os.system(f"google-drive-ocamlfuse -headless -id={creds.client_id} -secret={creds.client_secret} < /dev/null 2>&1 | grep URL")
-# vcode = getpass.getpass()
-# os.system(f"echo {vcode} | google-drive-ocamlfuse -headless -id={creds.client_id} -secret={creds.client_secret}")
+def authenticate():
+    auth.authenticate_user()
+    creds = GoogleCredentials.get_application_default()
+    os.system(f"google-drive-ocamlfuse -headless -id={creds.client_id} -secret={creds.client_secret} < /dev/null 2>&1 | grep URL")
+    vcode = getpass.getpass()
+    os.system(f"echo {vcode} | google-drive-ocamlfuse -headless -id={creds.client_id} -secret={creds.client_secret}")
